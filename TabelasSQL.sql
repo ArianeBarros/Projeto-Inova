ca1b3d6ca39aadf817c98d8c05fd1f711e340076
ArianeBarros <42739320+ArianeBarros@users.noreply.github.com>
2019-03-20 11:51:32 -0300

    20/03/2019

---
+++ TabelasSQL.sql
@@ -0,0 +1,44 @@
+
+
+
+create table UsuarioI9(
+cod int primary key,
+nome varchar(30),
+senha varchar(10),
+dataNascimento datetime,
+imagem ntext,
+pontuacao int
+)
+
+select * from UsuarioI9
+
+create table Evento(
+cod int primary key,
+quando datetime,
+localizacao ntext,
+descricao ntext
+)
+
+select * from Evento
+
+create table Realizacao(
+cod int primary key,
+descricao varchar(40),
+dia datetime
+)
+
+create table Noticia(
+cod int primary key,
+link ntext,
+imagem ntext,
+manchete varchar(30)
+)
+
+select * from Noticia
+
+create table Sonho(
+cod int primary key,
+descricao ntext
+)
+
+select * from Sonho
