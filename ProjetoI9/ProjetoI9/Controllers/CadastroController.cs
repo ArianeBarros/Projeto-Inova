e8ceb0754f578396253c535acc1f6994b034206f
ArianeBarros <42739320+ArianeBarros@users.noreply.github.com>
2019-04-30 11:53:45 -0300

    mnbnmbnm

--- ProjetoI9/ProjetoI9/Controllers/CadastroController.cs
+++ ProjetoI9/ProjetoI9/Controllers/CadastroController.cs
@@ -15,7 +15,7 @@ namespace ProjetoI9.Controllers
         {
             //UsuarioI9DAO dao = new UsuarioI9DAO();
             //IList<UsuarioI9> usu = dao.Lista();
-            //ViewBag.UsuarioI9 = usu;
+            //ViewBag.UsuarioI9 = new UsuarioI9();
             return View();
         }
 
@@ -24,19 +24,34 @@ namespace ProjetoI9.Controllers
             if (u != null && ModelState.IsValid)
             {
                 UsuarioI9DAO dao = new UsuarioI9DAO();
-                IList<UsuarioI9> usu = dao.Lista();
+                IList<UsuarioI9> usuLista = dao.Lista();
 
-                u.id = usu.Count() + 1;
+                bool existe = false;
+                foreach(UsuarioI9 umUsuario in usuLista)
+                {
+                    if (umUsuario.email == u.email)
+                    {
+                        ViewBag.Nome = u.nome;
+                        ViewBag.Nome = u.dataNascimento;
+                        ViewBag.Nome = u.senha;
+                        existe = true;
+                    }
+                                               
+                }
+                if(existe)
+                    return RedirectToAction("Index", "Cadastro");
+
+                u.id = usuLista.Count() + 1;
                 u.pontuacao = 0;
-                u.imagem = "~/Imagens/imgPerfil.jpg";
+                u.imagem = "/Imagens/imgPerfil.jpg";
 
                 dao.Adiciona(u);
 
-                return RedirectToAction("HomeLogado", "Login");
+                return RedirectToAction("HomeLogado", "Cadastro");
             }
             else
             {
-                return View("Index", "Cadastro");
+                return RedirectToAction("Index", "Cadastro");
             }                    
         }
     }
