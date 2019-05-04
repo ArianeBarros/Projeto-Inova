d43b386f8716a3985ff7537e8d3076f254c761b9
AnaSPires <42739437+AnaSPires@users.noreply.github.com>
2019-05-03 23:00:07 -0300

    03/05

---
+++ ProjetoI9/ProjetoI9/Controllers/ParetoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoI9.Controllers
+{
+    public class ParetoController : Controller
+    {
+        // GET: Pareto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
