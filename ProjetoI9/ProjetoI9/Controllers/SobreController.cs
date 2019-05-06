576740272e8bfd5650578905c04ca61eff1bfde0
ArianeBarros <42739320+ArianeBarros@users.noreply.github.com>
2019-05-06 11:55:50 -0300

    vcvbcvx

--- ProjetoI9/ProjetoI9/Controllers/SobreController.cs
+++ ProjetoI9/ProjetoI9/Controllers/SobreController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProjetoI9.DAO;
+using ProjetoI9.Models;
 
 namespace ProjetoI9.Controllers
 {
@@ -12,6 +14,14 @@ namespace ProjetoI9.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult SalvarDados(string img, string id, string cont)
+        {
+            UsuarioI9DAO dao = new UsuarioI9DAO();
+            UsuarioI9 usu = dao.BuscaPorId(Convert.ToInt32(id));
+
+            return RedirectToAction("Index", cont, new { usu.nome, usu.imagem, usu.id });
         }
     }
 }
