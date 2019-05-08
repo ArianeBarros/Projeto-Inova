86b32589bc67c1ad8847662e84defdee239d93e5
ArianeBarros <42739320+ArianeBarros@users.noreply.github.com>
2019-05-08 12:13:05 -0300

    gfgdgd

--- ProjetoI9/ProjetoI9/Controllers/PerfilController.cs
+++ ProjetoI9/ProjetoI9/Controllers/PerfilController.cs
@@ -20,6 +20,7 @@ namespace ProjetoI9.Controllers
             UsuarioI9DAO dao = new UsuarioI9DAO();
             UsuarioI9 usu = dao.BuscaPorId(Convert.ToInt32(id));
             usu.imagem = img;
+            
 
             return RedirectToAction("Index", "Principal", new { usu.nome, usu.imagem, usu.id });
         }
