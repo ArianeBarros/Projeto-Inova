7f069feea6a89fd66b48c70c4635daac98488cb3
Ana Pires <42739437+AnaSPires@users.noreply.github.com>
2019-05-13 12:10:14 -0300

    13/05

---
+++ ProjetoI9/ProjetoI9/Controllers/TermosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoI9.Controllers
+{
+    public class TermosController : Controller
+    {
+        // GET: Termos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
