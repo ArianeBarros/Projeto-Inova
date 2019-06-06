9477b22e417f2bff84edf1602d70b877fc9cda24
ArianeBarros <42739320+ArianeBarros@users.noreply.github.com>
2019-06-05 21:48:55 -0300

    cxcxcx

---
+++ ProjetoI9/ProjetoI9/Controllers/BastidoresController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjetoI9.Models;
+
+namespace ProjetoI9.Controllers
+{
+    public class BastidoresController : Controller
+    {
+        // GET: Bastidores
+        public ActionResult Index()
+        {
+            UsuarioI9 usuario = (UsuarioI9)Session["usuarioLogado"];
+
+            if (usuario != null)
+            {
+                ViewBag.UsuarioLogado = usuario;
+
+                return View();
+            }
+            else
+            {
+                UsuarioI9 usua = new UsuarioI9();
+                usua.imagem = "n";
+                ViewBag.UsuarioLogado = usua;
+                return View();
+            }
+        }
+
+        public ActionResult Erro()
+        {
+            Session["usuarioLogado"] = null;
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
